13ee3d71c863412845ee524bdd25a236de0b53c5
Ezaz Ahmed <ezaznahmed@gmail.com>
2024-03-05 00:20:32 +0600

    🛠️feat[backend]: Complete database integration

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
-import { Hono } from "hono";
+import { Hono } from 'hono';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
+import { users } from './db/schema';
 
-const app = new Hono();
+export type Env = {
+	DATABASE_URL: string;
+};
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+const app = new Hono<{ Bindings: Env }>();
+
+app.get('/', async (c) => {
+	try {
+		const sql = neon(c.env.DATABASE_URL);
+
+		const db = drizzle(sql);
+
+		const result = await db.select().from(users);
+
+		return c.json({ result });
+	} catch (error) {
+		console.log(error);
+
+		return c.json({ error }, 400);
+	}
 });
 
 export default app;
